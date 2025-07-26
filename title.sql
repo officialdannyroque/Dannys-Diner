7fa286bcdd0b5366340ef9d6b45fae0fdf77fa93
officialdannyroque <danny.roque12@gmail.com>
2025-07-26 12:36:15 -0700

    Update title.sql

--- title.sql
+++ title.sql
@@ -1,11 +1,9 @@
-WITH product_spend AS (SELECT customer_id, s.product_id, price * COUNT(s.product_id) AS spend
-						FROM sales s
-						INNER JOIN menu m
-						ON s.product_id = m.product_id
-						GROUP BY customer_id, s.product_id, price)
+## 1. What is the total amount each customer spent at the restaurant?
 
-
-SELECT customer_id, SUM(spend) AS total_spend
-FROM product_spend
-GROUP BY customer_id
-ORDER BY customer_id
+```sql
+SELECT customer_id,
+       SUM(price) AS total_sales
+FROM dannys_diner.menu
+INNER JOIN dannys_diner.sales USING(product_id)
+GROUP BY 1
+ORDER BY 1;
