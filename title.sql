e94b25a61d39e71491202d6dabf6c8cbcfc55943
officialdannyroque <danny.roque12@gmail.com>
2025-07-27 10:22:16 -0700

    Update and rename title.sql to solution.md

--- title.sql
+++
@@ -1,9 +0,0 @@
-## 1. What is the total amount each customer spent at the restaurant?
-
-```sql
-SELECT customer_id,
-       SUM(price) AS total_sales
-FROM dannys_diner.menu
-INNER JOIN dannys_diner.sales USING(product_id)
-GROUP BY 1
-ORDER BY 1;
